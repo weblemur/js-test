bef4a932b216e36a1338af81aaaa428eaa884413
Martin Gutfeldt <martingutfeldt@gmail.com>
2018-03-27 18:20:50 -0500

    Add goal spec

--- family-tree.spec.js
+++ family-tree.spec.js
@@ -1,10 +1,32 @@
 /* global describe it */
 const { expect } = require('chai');
 
-const { familyTree } = require('./family-tree');
+const { familyTree, people } = require('./family-tree');
 
 describe('familyTree', () => {
   it('handles empty inputs', () => {
     expect(familyTree([])).to.deep.equal([]);
+  });
+
+  xit('fully maps the given list of people', () => {
+    const result = [
+      { name: 'Sue', children: [
+        { name: 'John', children: [] },
+        { name: 'Garth', children: []}
+      ]},
+      { name: 'Sally', children: [
+        { name: 'Jill', children: [] }
+      ]},
+      { name: 'Alejandro', children: [
+        { name: 'Steve', children: [
+          { name: 'Jack', children: [
+            { name: 'Wayne', children: [] },
+            { name: 'Twinkle', children: [] }
+          ]}
+        ]},
+        { name: 'Peter', children: [] }
+      ]}
+    ];
+    expect(familyTree(people)).to.deep.equal(result);
   });
 });
